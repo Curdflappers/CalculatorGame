2d325b0c7822a6796c14a9bafd68466add52cc2f
Mark Wiemer <mwwiemer@outlook.com>
2018-06-13 12:13:06 -0500

    Refactor to use Regex so spaces aren't necessary

--- src/main/Rule.java
+++ src/main/Rule.java
@@ -1,16 +1,19 @@
 package main;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Rule {
     private int operand;
     /** The index associated with the operator */
     private int operator;
 
     public Rule(String rule) {
-        String[] input = rule.split(" ");
-        setOperator(toOperator(input[0]));
-        if (input.length > 1) {
-            setOperand(input[1]);
-        }
+        Matcher matcher = Pattern.compile("\\d+").matcher(rule);
+        boolean hasInt = matcher.find();
+        String operator = hasInt ? rule.substring(0, matcher.start()) : rule;
+        setOperator(toOperator(operator));
+        if (hasInt) { setOperand(matcher.group()); }
     }
 
     /**
@@ -22,7 +25,7 @@ public class Rule {
      * @throws RuntimeException when synonym is not recognized
      */
     private static int toOperator(String synonym) {
-        switch (synonym) {
+        switch (synonym.trim()) {
             case ("add"):
             case ("plus"):
             case ("+"):
@@ -44,6 +47,10 @@ public class Rule {
     private void setOperand(String operand) {
         try {
             this.operand = Integer.parseInt(operand);
+            if (this.operand > Config.NUM_OPERANDS) {
+                throw new RuntimeException(
+                    "Operand out of range: " + this.operand);
+            }
         } catch (NumberFormatException e) {
             System.out.println(
                 "Unexpected NumberFormatException in Rule.setOperand");
