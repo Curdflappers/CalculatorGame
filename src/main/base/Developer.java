aaefcdd6802fda924b5c43e84f3bf27ad9f81e55
Mark Wiemer <markwiemer@outlook.com>
2019-03-27 20:34:49 +0100

    Allow developer to change name of test case file
    Before, they would either overwrite the file or lose the test case.

--- src/main/base/Developer.java
+++ src/main/base/Developer.java
@@ -30,24 +30,79 @@ public class Developer {
         System.out.print("Save test case (y/n): ");
         String saveResponse = scanner.nextLine();
         if (saveResponse.length() == 0 || saveResponse.charAt(0) == 'y') {
-            System.out.print("Filename (\".cgl\" will be added): ");
-            String filename =
-                Config.TESTCASES_PATH + "/" + scanner.nextLine() + ".cgl";
-
-            if (fileExists(filename)) {
-                System.out
-                    .print("That file already exists. Overwrite it? (y/n): ");
-                if (scanner.nextLine().charAt(0) == 'y') {
-                    saveTestCase(filename, game, solution);
+            do {
+                System.out.print("Filename (\".cgl\" will be added): ");
+                String filename =
+                    Config.TESTCASES_PATH + "/" + scanner.nextLine() + ".cgl";
+                if (fileExists(filename)) {
+                    switch (promptOverwrite(scanner)) {
+                        case 'o':
+                            saveTestCase(filename, game, solution);
+                            return;
+                        case 'd':
+                            continue;
+                        case 's':
+                            skipTestCase();
+                            return;
+                    }
                 } else {
-                    skipTestCase();
+                    saveTestCase(filename, game, solution);
+                    return;
                 }
-            } else {
-                saveTestCase(filename, game, solution);
-            }
+            } while (true);
         } else {
             skipTestCase();
         }
+    }
+
+    /**
+     * Returns what the save option selected by the user when the file already
+     * exists. 'o' for overwrite, 'd' for save under different name, 's' for
+     * skip this test case.
+     *
+     * Loops if the user does not enter one of those three options.
+     *
+     * Returns 'o', 'd', or 's'
+     */
+    private static char promptOverwrite(Scanner scanner) {
+        char firstChar = '\n';
+
+        while (!validOverwriteResponse(firstChar)) {
+            System.out
+                .print(
+                    "That file already exists. [O]verwrite it? Save under [D]ifferent name? [S]kip this test case? (o/d/s): "
+                );
+
+            String response = scanner.nextLine();
+
+            if (response.length() == 0) {
+                System.out.println("Please enter a response.");
+                continue;
+            }
+
+            if (response.length() != 1) {
+                System.out.println("Please enter a one-character response");
+                continue;
+            }
+
+            firstChar = Character.toLowerCase(response.charAt(0));
+
+            if (!validOverwriteResponse(firstChar)) {
+                System.out
+                    .println(
+                        "Please enter 'o', 'd', or 's' only (case-insensitive)"
+                    );
+            }
+        }
+
+        return firstChar;
+    }
+
+    /**
+     * Returns whether the given character is a valid response for the overwrite prompt
+     */
+    private static boolean validOverwriteResponse(char c) {
+        return c == 'o' || c == 'd' || c == 's';
     }
 
     private static boolean fileExists(String filename) {
