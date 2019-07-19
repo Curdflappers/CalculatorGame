bfcf585ea41bb4b8578bb194c88be384edffb5fd
Mark Wiemer <markwiemer@outlook.com>
2019-07-19 11:34:49 -0400

    Refactor level tests, decouple logic

--- src/test/calculatorgame/LevelTests.java
+++ src/test/calculatorgame/LevelTests.java
@@ -1,12 +1,8 @@
 package calculatorgame;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 import org.junit.jupiter.api.Test;
 
@@ -15,89 +11,27 @@ import brutesolver.State;
 
 public class LevelTests {
 
-    private static CalculatorGame game = null;
-    private static String expectedSolutionString = null;
-
-    private static void loadLevel(String level) {
-        File file = new File(Config.TESTCASES_PATH + "/" + level);
-        Scanner input = null;
-        try {
-            input = new Scanner(file);
-
-            int value = Integer.parseInt(input.nextLine());
-            int goal = Integer.parseInt(input.nextLine());
-            int moves = Integer.parseInt(input.nextLine());
-
-            String currentRuleString;
-            List<Rule> rules = new ArrayList<>();
-
-            // While the current rule is not the empty string
-            while (!(currentRuleString = input.nextLine()).equals("")) {
-                Rule rule = Rule.ruleFromString(currentRuleString);
-                rules.add(rule);
-            }
-
-            boolean portalsPresent = input.nextLine().charAt(0) == 'y';
-
-            int leftPortal = -1;
-            int rightPortal = -1;
-
-            if (portalsPresent) {
-                leftPortal = Integer.parseInt(input.nextLine());
-                rightPortal = Integer.parseInt(input.nextLine());
-            }
-
-            expectedSolutionString = "";
-
-            while (input.hasNextLine()) {
-                expectedSolutionString += input.nextLine() + "\n";
-            }
-
-            int[] portals = null;
-            if (portalsPresent) portals = new int[] {
-                leftPortal, rightPortal
-            };
-
-            game =
-                CalculatorGame
-                    .generateGame(
-                        value,
-                        goal,
-                        moves,
-                        rules.toArray(new Rule[rules.size()]),
-                        portals
-                    );
-
-        } catch (FileNotFoundException e) {
-            game = null;
-            System.out.print("FILE NOT FOUND");
-        } finally {
-            if (input != null) input.close();
-        }
-    }
-
     @Test
     void passesAllLevels() {
-
-        boolean failed = false;
-
-        for (String testCase : testCases()) {
+        boolean success = true;
+        for (String testCase : Helpers.testCases()) {
             System.out.print("Testing " + testCase + ": ");
-            loadLevel(testCase);
+            String expectedSolution = Helpers.expectedSolution(testCase);
+            CalculatorGame game = Helpers.loadLevel(testCase);
 
             if (game == null) {
-                failed = true;
-                System.out.println(" FAILED");
+                success = false;
+                System.out.println("FAILED: Could not load level");
                 continue;
             }
 
             List<State> solutionStates = Solver.solve(game);
             String solutionString = State.allTransitions(solutionStates);
 
-            if (!solutionString.equals(expectedSolutionString)) {
-                failed = true;
+            if (!solutionString.equals(expectedSolution)) {
+                success = false;
                 System.out.println("FAILED");
-                System.out.println("Expected:\n" + expectedSolutionString);
+                System.out.println("Expected:\n" + expectedSolution);
                 System.out.println("Actual:\n" + solutionString);
                 continue;
             }
@@ -105,17 +39,6 @@ public class LevelTests {
             System.out.println("PASSED");
         }
 
-        assertFalse(failed);
-    }
-
-    private static List<String> testCases() {
-        final File folder = new File(Config.TESTCASES_PATH);
-        List<String> filenames = new ArrayList<>();
-
-        for (final File fileEntry : folder.listFiles()) {
-            filenames.add(fileEntry.getName());
-        }
-
-        return filenames;
+        assertTrue(success);
     }
 }
