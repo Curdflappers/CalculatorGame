0cfb524d6271acc532ced0703b2a7f9823555594
Mark Wiemer <markwiemer@outlook.com>
2019-08-01 22:55:01 -0500

    Add basic level playing logic

--- src/main/java/com/mathwithmark/calculatorgamesolver/main/Play.java
+++ src/main/java/com/mathwithmark/calculatorgamesolver/main/Play.java
@@ -1,23 +1,31 @@
 package com.mathwithmark.calculatorgamesolver.main;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import com.mathwithmark.calculatorgamesolver.calculatorgame.CalculatorGame;
 import com.mathwithmark.calculatorgamesolver.calculatorgame.Config;
+import com.mathwithmark.calculatorgamesolver.calculatorgame.Rule;
 import com.mathwithmark.calculatorgamesolver.yaml.Serialize;
 
 public class Play {
     private static final String GOODBYE = "Goodbye!";
+    private static final String LEVEL = "Level %d";
+    private static final String LEVEL_WON =
+        "Congratulations, you beat the level!";
     private static final String NEXT_LEVEL = "Next level? (%c/%c): ";
+    private static final String RULE = "Enter rule to apply: ";
+    private static final String VALID_RULE = "Please enter a valid rule.";
     private static final String WELCOME = "Welcome to Calculator: The Game!";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int levelIndex = 1;
-        
+
         System.out.println(WELCOME);
         do {
-            boolean completed = playLevel(levelIndex);
+            boolean completed = playLevel(scanner, levelIndex);
             if (!completed) break;
             levelIndex++;
         } while (nextLevel(scanner));
@@ -49,15 +57,35 @@ public class Play {
         return path;
     }
 
+    private static List<String> ruleStrings(CalculatorGame level) {
+        List<String> ruleStrings = new ArrayList<String>();
+        for (Rule rule : level.getRules()) {
+            ruleStrings.add(rule.toString());
+        }
+        return ruleStrings;
+    }
+
     /**
      * Loads the given level and allows the user to play.
      * @param levelIndex the index of the level to load
      * @return true if the user completed the level, false otherwise
      */
-    private static boolean playLevel(int levelIndex) {
+    private static boolean playLevel(Scanner scanner, int levelIndex) {
         String levelPath = levelPathFrom(levelIndex);
         CalculatorGame level = Serialize.loadLevel(levelPath);
-        System.out.println(level);
+        System.out.printf(LEVEL + "\n", levelIndex);
+        do {
+            System.out.println(level);
+            System.out.print(RULE);
+            String inputRuleString = scanner.nextLine().trim();
+            if (!ruleStrings(level).contains(inputRuleString)) {
+                System.out.println(VALID_RULE);
+                continue;
+            }
+            Rule rule = Rule.ruleFromString(inputRuleString);
+            level = rule.apply(level);
+        } while (!level.isWon());
+        System.out.println(LEVEL_WON);
         return true;
     }
 
