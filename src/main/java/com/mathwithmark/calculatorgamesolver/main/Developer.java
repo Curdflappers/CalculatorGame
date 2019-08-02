4b83c81f38e6e597e654f24908462bad8b403adc
Mark Wiemer <markwiemer@outlook.com>
2019-08-01 21:38:37 -0500

    Change long outputs to prompt variables

--- src/main/java/com/mathwithmark/calculatorgamesolver/main/Developer.java
+++ src/main/java/com/mathwithmark/calculatorgamesolver/main/Developer.java
@@ -11,6 +11,24 @@ import com.mathwithmark.calculatorgamesolver.calculatorgame.Config;
 import com.mathwithmark.calculatorgamesolver.yaml.Serialize;
 
 public class Developer {
+    private static final String QUIT_PROMPT =
+        "Enter '"
+            + Config.QUIT
+            + "' to quit. Enter anything else to continue: ";
+    private static final String TEST_CASE_NAME_PROMPT =
+        "Filename (\""
+            + Config.TEST_CASE_FILE_EXTENSION
+            + "\" will be added): ";
+    private static final String SAVE_TEST_CASE_PROMPT =
+        "Save test case (y/n): ";
+    private static final String FILE_EXISTS_PROMPT =
+        "That file already exists. "
+            + "[O]verwrite it? "
+            + "Save under [D]ifferent name? "
+            + "[S]kip this test case? (o/d/s): ";
+    private static final String VALID_FILE_EXISTS_RESPONSE_PROMPT =
+        "Please enter 'o', 'd', or 's' only (case-insensitive)";
+
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         do {
@@ -20,12 +38,7 @@ public class Developer {
             List<String> solution = solutions.get(0);
             Main.printSolutions(solutions);
             promptSaveTestCase(scanner, Main.getCalculatorGame(), solution);
-            System.out
-                .print(
-                    "Enter '"
-                        + Config.QUIT
-                        + "' to quit. Enter anything else to continue: "
-                );
+            System.out.print(QUIT_PROMPT);
         } while (!scanner.nextLine().equalsIgnoreCase(Config.QUIT));
     }
 
@@ -40,16 +53,11 @@ public class Developer {
         List<String> solution
     )
         throws IOException {
-        System.out.print("Save test case (y/n): ");
+        System.out.print(SAVE_TEST_CASE_PROMPT);
         String saveResponse = scanner.nextLine();
         if (saveResponse.length() == 0 || saveResponse.charAt(0) == 'y') {
             do {
-                System.out
-                    .print(
-                        "Filename (\""
-                            + Config.TEST_CASE_FILE_EXTENSION
-                            + "\" will be added): "
-                    );
+                System.out.print(TEST_CASE_NAME_PROMPT);
                 String filePath =
                     Config.TEST_CASES_PATH
                         + scanner.nextLine()
@@ -86,35 +94,22 @@ public class Developer {
      */
     private static char promptOverwrite(Scanner scanner) {
         char firstChar = '\n';
-
         while (!validOverwriteResponse(firstChar)) {
-            System.out
-                .print(
-                    "That file already exists. [O]verwrite it? Save under [D]ifferent name? [S]kip this test case? (o/d/s): "
-                );
-
+            System.out.print(FILE_EXISTS_PROMPT);
             String response = scanner.nextLine();
-
             if (response.length() == 0) {
                 System.out.println("Please enter a response.");
                 continue;
             }
-
             if (response.length() != 1) {
                 System.out.println("Please enter a one-character response");
                 continue;
             }
-
             firstChar = Character.toLowerCase(response.charAt(0));
-
             if (!validOverwriteResponse(firstChar)) {
-                System.out
-                    .println(
-                        "Please enter 'o', 'd', or 's' only (case-insensitive)"
-                    );
+                System.out.println(VALID_FILE_EXISTS_RESPONSE_PROMPT);
             }
         }
-
         return firstChar;
     }
 
